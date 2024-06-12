34cf1042d09a1307847e5aef87d16dbbbba9f7d3
1565433859 <1565433859@qq.com>
2024-06-12 13:12:46 +0800

    enable gitignore

---
+++ c++_program/add.cpp
@@ -0,0 +1,6 @@
+
+#include"global.h"
+
+int add(int a,int b){
+	return a + b;
+}
---
+++ c++_program/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"global.h"
+
+using namespace std;
+
+int main() {
+	cout << add(1, 2);
+
+
+	return 0;
+}
